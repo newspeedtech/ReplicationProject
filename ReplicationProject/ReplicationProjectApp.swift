16b250c7d9e70012a6425744e1ebac79f63e5ed4
newspeed <mack76801@yahoo.com>
2024-04-13 18:06:03 -0500

    Initial Commit

---
+++ ReplicationProject/ReplicationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReplicationProjectApp.swift
+//  ReplicationProject
+//
+//  Created by Marla Sumner on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ReplicationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
